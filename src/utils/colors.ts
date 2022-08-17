78cf73bba900717a60e23902da39dd6bd9060b54
cesarvargasjr <cesar_vargas_jr@hotmail.com>
2022-08-16 22:48:06 -0300

    #02

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,9 +2,6 @@ export default {
     white: '#FFFFFF',
     black: '#000000',
     purple: '#373AAD',
-    
-    
-    
     purpleDark: '#888BF0',
     blackNeutro: '#666666',
     LightBlue: '#29abe2',
@@ -12,27 +9,5 @@ export default {
     orange: '#FF824D',
     yellow: '#FFED00',
     green: '#16C153',
-
-
-
-
-    heading: '#353535',
-    input: '#353535',
-    grayCheckbox: '#8C8C8C',
-    gray: '#AAAFB9',
-    mediumGray: '#91AAB5',
-    link: '#5ECAEE',
-    blue: '#29ABE2',
-    lightBlue: '#E5F5FC',
-    lightBlue2: '#1584B4',
-    labelInput: '#BEDFED',
-    sucess: '#2A9E3E',
-    warning: '#C5AB00',
-    alertWarning: '#EF504B',
-    secondGreen: '#2A9E3E',
-    coral: '#EF504B',
-    redError: '#EF504B',
-    redDead: '#E82B36',
-    darkGray: '#EBEBEB',
   };
   
